ccc6d913a9ca8de3a9711202057725463288518a
NightUmbrella <githubumbrella@gmail.com>
2023-07-14 20:36:39 +0500

    find bag

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import BackgroundProvider from './Slice/BgSlice'
-import AudioSLice from "./Slice/AudioSLice";
+import AudioSLice from "./Slice/AudioSlice";
 export const store = configureStore({
     reducer:{
         background:BackgroundProvider,
